bb1be09e9a721c709c71e44deb8d557f143e22d8
jdjiko00 <jdjiko00@fesb.hr>
2026-06-30 18:58:02 +0200

    Spring Boot structure

---
+++ src/main/java/com/diko/productmiddleware/ProductMiddlewareApplication.java
@@ -0,0 +1,13 @@
+package com.diko.productmiddleware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductMiddlewareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductMiddlewareApplication.class, args);
+	}
+
+}
